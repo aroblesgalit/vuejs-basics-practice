12ab7e51f9bfe94a0077b761739764a9fc196163
Alvin Galit <aroblesgalit@gmail.com>
2021-10-29 15:14:48 -0600

    Learned Vue templating and data binding to display info, and how to add functionality and interactivity with Vue directives and event handling

--- Project-Files/s2v3/app.js
+++ Project-Files/s2v3/app.js
@@ -1,8 +1,15 @@
 const book = new Vue({
-    el: '#book',
-    data: {
-      title: 'The Sirens of Titan',
-      author: 'Kurt Vonnegut',
-      summary: 'This is a summary of the Sirens of Titan.',
+  el: '#book',
+  data: {
+    title: 'The Sirens of Titan',
+    author: 'Kurt Vonnegut',
+    summary: 'This is a summary of the Sirens of Titan.',
+    showDetail: false
+  },
+  methods: {
+    // Avoid using arrow functions
+    toggleDetails: function () {
+      this.showDetail = !this.showDetail
     }
-});
+  }
+})
