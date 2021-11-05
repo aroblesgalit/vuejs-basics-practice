baca8ac647d3e658c23ae38e443ab8f728c3da7c
Alvin Galit <aroblesgalit@gmail.com>
2021-11-05 14:53:06 -0600

    Pseudo coded tasks

--- js-vue-flashcards/s4v1/app.js
+++ js-vue-flashcards/s4v1/app.js
@@ -1,33 +1,38 @@
 const cards = [
-    {
-      front: 'The "First Computer Programmer"',
-      back: 'Ada Lovelace',
-      flipped: false,
-    },
-    {
-      front: 'Invented the "Clarke Calculator"',
-      back: 'Edith Clarke',
-      flipped: false,
-  
-    },
-    {
-      front: 'Famous World War II Enigma code breaker',
-      back: 'Alan Turing',
-      flipped: false,
-    },
-    {
-      front: 'Created satellite orbit analyzation software for NASA',
-      back: 'Dr. Evelyn Boyd Granville',
-      flipped: false,
-    },
-  ]; 
+  {
+    front: 'The "First Computer Programmer"',
+    back: 'Ada Lovelace',
+    flipped: false
+  },
+  {
+    front: 'Invented the "Clarke Calculator"',
+    back: 'Edith Clarke',
+    flipped: false
+  },
+  {
+    front: 'Famous World War II Enigma code breaker',
+    back: 'Alan Turing',
+    flipped: false
+  },
+  {
+    front: 'Created satellite orbit analyzation software for NASA',
+    back: 'Dr. Evelyn Boyd Granville',
+    flipped: false
+  }
+]
 
-  new Vue({
-    el: '#flashcard-app',
-    data: {
-      cards: cards,
-    },
-    methods: {
+// Display our data
+// On click: flip cards back and forth
+// Get info for new cards from the user
+// Add new card when user hits enter or clicks button
+// Delete cards
+// Animate card flip
+// Display an error message if form fields are blank
 
-    }
-  });
+new Vue({
+  el: '#flashcard-app',
+  data: {
+    cards: cards
+  },
+  methods: {}
+})
