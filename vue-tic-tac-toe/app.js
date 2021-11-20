659e5f3987f23bbd5d9c65d7c800853a6f489eef
Alvin Galit <aroblesgalit@gmail.com>
2021-11-19 23:17:39 -0700

    Created new vue instance with data properties for title, instructions and playerTurn

---
+++ vue-tic-tac-toe/app.js
@@ -0,0 +1,11 @@
+// Create data properties for title, instructions, playerTurn
+// Add properties to vue template
+
+new Vue({
+  el: '#app',
+  data: {
+    title: 'tic tac toe',
+    instructions: 'x goes first',
+    playerTurn: 'x'
+  }
+})
