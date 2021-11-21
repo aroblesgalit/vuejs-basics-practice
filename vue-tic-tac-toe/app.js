96a68df696b8355cf0d764a32cc4aa1286fd9ca4
Alvin Galit <aroblesgalit@gmail.com>
2021-11-20 18:12:47 -0700

    Checked for winner after making a mark and showing the winning copy when someone wins

--- vue-tic-tac-toe/app.js
+++ vue-tic-tac-toe/app.js
@@ -15,28 +15,42 @@ new Vue({
       // Mark cell accordingly
       if (!event.target.innerText && this.turns < 9) {
         event.target.append(this.playerTurn)
-        if (this.playerTurn === 'x' && this.turns < 8) {
-          this.playerTurn = 'o'
-          this.instructions = "o's turn"
-        } else if (this.playerTurn === 'o' && this.turns < 8) {
-          this.playerTurn = 'x'
-          this.instructions = "x's turn"
-        } else {
-          this.instructions = '??? wins!'
-        }
-        // if (this.playerTurn === 'x') {
-        //   event.target.append('x')
+        // if (this.playerTurn === 'x' && this.turns < 8) {
         //   this.playerTurn = 'o'
         //   this.instructions = "o's turn"
-        // } else {
-        //   event.target.append('o')
+        // } else if (this.playerTurn === 'o' && this.turns < 8) {
         //   this.playerTurn = 'x'
         //   this.instructions = "x's turn"
+        // } else {
+        //   this.instructions = '??? wins!'
         // }
-        this.turns += 1
       }
-
       // Check if winner
+      const cells = document.querySelectorAll('.cell')
+      const cellMarks = []
+      cells.forEach(function (cell) {
+        cellMarks.push(cell.innerText)
+      })
+      console.log(cellMarks)
+      if (this.turns >= 5) {
+        if (
+          (cellMarks[0] === cellMarks[1] && cellMarks[0] === cellMarks[2]) ||
+          (cellMarks[3] === cellMarks[4] && cellMarks[3] === cellMarks[5]) ||
+          (cellMarks[6] === cellMarks[7] && cellMarks[6] === cellMarks[8])
+        ) {
+          this.winner = this.playerTurn
+        }
+      }
+      if (this.playerTurn === 'x' && this.turns < 8) {
+        this.playerTurn = 'o'
+        this.instructions = "o's turn"
+      } else if (this.playerTurn === 'o' && this.turns < 8) {
+        this.playerTurn = 'x'
+        this.instructions = "x's turn"
+      } else {
+        this.instructions = '??? wins!'
+      }
+      this.turns += 1
     },
     reset: function () {
       this.instructions = 'x goes first'
